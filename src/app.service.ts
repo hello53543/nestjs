891bab7fc788785a5f4be58a78452deb8e96709f
Lutz Timeo, GHR-POD-NEX-34 <Timeo.Lutz@swisscom.com>
2025-04-25 13:11:31 +0200

    second change

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,11 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { Book, books } from './FakeDatabase';
 import { title } from 'process';
  
-// Macht Klasse als Dienst verfügbar
 @Injectable()
 export class BooksService {
  
-  // Gibt alle Bücher als ein Array zurück
   getAllBooks(): Book[] {
     return books;
   }
@@ -15,7 +13,6 @@ export class BooksService {
     return books.find((book) => book.id === bookId);
   }
  
-  // Nimmt ein Buchobjekt entgegen und erstellt ein neues Buch.
   create(book: Partial<Book>): Book {
     const newID = books[books.length - 1].id + 1;
  
@@ -26,12 +23,10 @@ export class BooksService {
       publicationYear: book.publicationYear ?? 0,
     };
  
-     // Fügt das neue Buch zum Array hinzu, gibt es zurück.
     books.push(newBook);
     return newBook;
   }
 
-  // Aktualisiert ein Buch anhand der ID.
   update(bookID: number, updateBookFields: Partial<Book>): Book | undefined {
     const currentBook = books.find((book) => book.id === bookID);
     const updatedBook = {
@@ -40,15 +35,12 @@ export class BooksService {
       author: updateBookFields.author ?? currentBook.author,
       publicationYear: updateBookFields.publicationYear ?? currentBook.publicationYear,
     }; 
-    // Ersetzt das alte Buch im Array durch das aktualisierte Buch.
     books[bookID - 1] = updatedBook;
 
     return updatedBook;
   }
 
-  // Löscht ein Buch anhand der ID.
   delete(bookID: number): Book[] {
-    // Entfernt das Buch mit der gegebenen ID aus dem Array.
    books.splice(bookID-1, 1)
     return books;
   }
